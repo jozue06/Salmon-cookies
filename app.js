c6bf99efe20f4a2f4a3054da168dac304dbd6bb1
Jozue06 <jozue06@gmail.com>
2018-01-24 01:04:31 -0800

    simple tables are working, coloring the main page, css, columns. still jumbled

--- app.js
+++ app.js
@@ -23,12 +23,12 @@ var alki = {
   //creates the table with the info from "this.sales per h"
   makeTable: function(){
     this.makeSalesPerH();
-    var storeEle = document.getElementById('stores');
+    var storeEle = document.getElementById('store1');
     var tableEle = document.createElement('ul');
     //takes biz hours info and sales per that biz hour and contactinates them in the LI's
     for (var i = 0; i < bizHours.length; i++){
       var rowEle = document.createElement('li');
-      rowEle.textContent = bizHours[i] + ' : ' + this.salesPerH[i] + ' cookies';
+      rowEle.textContent = bizHours[i] + ': ' + this.salesPerH[i] + ' cookies';
       tableEle.appendChild(rowEle);
     }
     //makes a seperate LI for just total sales all day
@@ -42,10 +42,147 @@ var alki = {
 alki.makeTable(); //calls the elements, store and all its info, and all the functions inside the object
 
 
+var pikePlace  = {
+  name: '1st and Pike',
+  minCustomer: 23,
+  maxCustomer: 65,
+  avgCookie: 6.3,
+  salesPerH: [],
+  totalSales: 0,
+  randomCust: function(){
+    return Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer;
+  },
+  makeSalesPerH: function(){
+    for (var i = 0; i < bizHours.length; i++ ){
+      var random = Math.floor(this.randomCust() * this.avgCookie);
+      this.salesPerH.push(random);
+      this.totalSales += random;
+    }
+  },
+  makeTable: function(){
+    this.makeSalesPerH();
+    var storeEle = document.getElementById('store2');
+    var tableEle = document.createElement('ul');
+    for (var i = 0; i < bizHours.length; i++){
+      var rowEle = document.createElement('li');
+      rowEle.textContent = bizHours[i] + ': ' + this.salesPerH[i] + ' cookies';
+      tableEle.appendChild(rowEle);
+    }
+    var totalEle = document.createElement('li');
+    totalEle.textContent = 'Total: ' + this.totalSales;
+    tableEle.appendChild(totalEle);
+    storeEle.textContent = this.name;
+    storeEle.appendChild(tableEle);
+  }
+};
+pikePlace.makeTable();
 
 
+var seaTac  = {
+  name: 'SeaTac Airport',
+  minCustomer: 3,
+  maxCustomer: 24,
+  avgCookie: 1.2,
+  salesPerH: [],
+  totalSales: 0,
+  randomCust: function(){
+    return Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer;
+  },
+  makeSalesPerH: function(){
+    for (var i = 0; i < bizHours.length; i++ ){
+      var random = Math.floor(this.randomCust() * this.avgCookie);
+      this.salesPerH.push(random);
+      this.totalSales += random;
+    }
+  },
+  makeTable: function(){
+    this.makeSalesPerH();
+    var storeEle = document.getElementById('store3');
+    var tableEle = document.createElement('ul');
+    for (var i = 0; i < bizHours.length; i++){
+      var rowEle = document.createElement('li');
+      rowEle.textContent = bizHours[i] + ': ' + this.salesPerH[i] + ' cookies';
+      tableEle.appendChild(rowEle);
+    }
+    var totalEle = document.createElement('li');
+    totalEle.textContent = 'Total: ' + this.totalSales;
+    tableEle.appendChild(totalEle);
+    storeEle.textContent = this.name;
+    storeEle.appendChild(tableEle);
+  }
+};
+seaTac.makeTable();
+
+var seaCen  = {
+  name: 'Seattle Center',
+  minCustomer: 11,
+  maxCustomer: 38,
+  avgCookie: 3.7,
+  salesPerH: [],
+  totalSales: 0,
+  randomCust: function(){
+    return Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer;
+  },
+  makeSalesPerH: function(){
+    for (var i = 0; i < bizHours.length; i++ ){
+      var random = Math.floor(this.randomCust() * this.avgCookie);
+      this.salesPerH.push(random);
+      this.totalSales += random;
+    }
+  },
+  makeTable: function(){
+    this.makeSalesPerH();
+    var storeEle = document.getElementById('store4');
+    var tableEle = document.createElement('ul');
+    for (var i = 0; i < bizHours.length; i++){
+      var rowEle = document.createElement('li');
+      rowEle.textContent = bizHours[i] + ': ' + this.salesPerH[i] + ' cookies';
+      tableEle.appendChild(rowEle);
+    }
+    var totalEle = document.createElement('li');
+    totalEle.textContent = 'Total: ' + this.totalSales;
+    tableEle.appendChild(totalEle);
+    storeEle.textContent = this.name;
+    storeEle.appendChild(tableEle);
+  }
+};
+seaCen.makeTable();
 
 
+var capHill  = {
+  name: 'Capitol Hill',
+  minCustomer: 20,
+  maxCustomer: 38,
+  avgCookie: 2.3,
+  salesPerH: [],
+  totalSales: 0,
+  randomCust: function(){
+    return Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer;
+  },
+  makeSalesPerH: function(){
+    for (var i = 0; i < bizHours.length; i++ ){
+      var random = Math.floor(this.randomCust() * this.avgCookie);
+      this.salesPerH.push(random);
+      this.totalSales += random;
+    }
+  },
+  makeTable: function(){
+    this.makeSalesPerH();
+    var storeEle = document.getElementById('store5');
+    var tableEle = document.createElement('ul');
+    for (var i = 0; i < bizHours.length; i++){
+      var rowEle = document.createElement('li');
+      rowEle.textContent = bizHours[i] + ': ' + this.salesPerH[i] + ' cookies';
+      tableEle.appendChild(rowEle);
+    }
+    var totalEle = document.createElement('li');
+    totalEle.textContent = 'Total: ' + this.totalSales;
+    tableEle.appendChild(totalEle);
+    storeEle.textContent = this.name;
+    storeEle.appendChild(tableEle);
+  }
+};
+capHill.makeTable();
 
 /*
 var bizHours = ['6am', '7am', '8am', '9am','10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm','6pm','7pm', '8pm'];
