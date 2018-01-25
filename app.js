e364e8f3ccd67dae16e4726242c868e80decfdb4
Jozue06 <jozue06@gmail.com>
2018-01-24 21:02:12 -0800

    starting constructors

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 var bizHours = ['6am', '7am', '8am', '9am','10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm','6pm','7pm'];
 //the first store info object:
 
+//constructors create new objects like a factory.
+
 var alki = {
   name: 'Alki',
   minCustomer: 2,
@@ -22,7 +24,7 @@ var alki = {
     }
   },
 
-  
+
   //creates the table with the info from "this.sales per h"
 
   makeTable: function(){
