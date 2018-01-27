326e0f38ab624dcdfcdc5eaf065ed2c75c8a6650
Jozue06 <jozue06@gmail.com>
2018-01-27 09:11:40 -0800

    start of lab, making new branch

--- rachelapp.js
+++ rachelapp.js
@@ -1,8 +1,5 @@
 'use strict';
-//constructors! think of it like a specialized machine in a factory that creates new objects
-//prototypes are the functionality that the machine adds to the objects
 
-//we need to define our hours and store them
 var bizHours = [
   '6am',
   '7am',
@@ -21,41 +18,30 @@ var bizHours = [
   '8pm'
 ];
 
-
-
-//we need to create a place for our list of all locations to be placed whenwe create them
 var locations = [];
-
-//we need to define a variable that will be dynamic depending on how many cookies
-var totalCookiesByHour = 0;
-//totalTotal
 var netTotal = 0;
+var totalCookiesByHour = 0;
 
 //constructor function begin with an Uppercase letter
-function makeStands() {
+function MakeStands() {
   new MakeLocation('First and Pike', 23, 65, 6.3);
   new MakeLocation('SeaTac Airport', 3, 24, 1.2);
   new MakeLocation('Seattle Center', 11, 38, 3.7);
   new MakeLocation('Capotol Hill', 20, 38, 2.3);
   new MakeLocation('Alki', 2, 16, 4.6);
 }
-makeStands();
+MakeStands();
 
 function MakeLocation( name, minCustPerHour, maxCustPerHour, avgCookies) {
-  //a function called MakeLocation will be a template for creating new objects that represent other locations
-  //the statements in this function add properties and or methods to the objects
-  //the this keyword is used instead of the object name to indicate that th property or method belongs to the object that this function creates
-  //this function has four parameters and each one sets the value of a property in the object
   this.name = name;
   this.minCustPerHour = minCustPerHour;
   this.maxCustPerHour = maxCustPerHour;
   this.avgCookies = avgCookies;
-  //why is this an array?
   this.randCustByHour = [];
   this.cookiesByHour = [];
   this.totalSales = 4;
   locations.push(this);
-
+  
   function render(){
   }
   var storeSection = document.getElementById('store1');
@@ -64,7 +50,7 @@ function MakeLocation( name, minCustPerHour, maxCustPerHour, avgCookies) {
   td.textContent = this.name;
   row.appendChild(td);
 
-  for (var j = 0; j <bizHours.length; j++) {
+  for (var j = 0; j < bizHours.length; j++) {
     var tdHourly = document.createElement('td');
     var minCust = document.createElement('th');
     minCust.textContent = this.minCustPerHour;
@@ -93,13 +79,12 @@ function MakeLocation( name, minCustPerHour, maxCustPerHour, avgCookies) {
     row.appendChild(td);
 
     for (var i = 0; i < bizHours.length; i++) {
-      // var td2 = document.createElement('th');
-      // td2.textContent = bizHours[i];
+      var td2 = document.createElement('th');
+      td2.textContent = bizHours[i];
       row.appendChild(td);
     }
   }
   generateTableHeading();
- 
 
   //using the this keyword in front of the method to show that the method belongs to the object that this function creates
   // this.calcRandCustByHour = function() {
